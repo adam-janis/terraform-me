6e2d6b4c5abeaa6ed4ed3d0c821e349e09af0488
Adam Janiš <adam.janis@gmail.com>
2018-10-25 08:08:21 -0700

    Create providers.tf

---
+++ remote-state/providers.tf
@@ -0,0 +1,7 @@
+#terraform {
+#  backend "gcs" {
+#    bucket = "terraform-me-MY_BUCKET" # change MY_BUCKET to your created bucket
+#    prefix = "terraform/state"
+#    region = "europe-west1"
+#  }
+#}
